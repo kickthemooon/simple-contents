9b287f72d181f133228019ba10d84d9e87ae669b
adnan <kickthemooon@gmail.com>
2023-04-12 03:26:16 +0200

    add npm publish to ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,9 @@ export default defineConfig({
     build: {
         lib: {
             formats: ['umd', 'es'],
-            // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'SimpleContents',
-            // the proper extensions will be added
-            fileName: 'simple-contents',
+            fileName: 'index',
         }
     }
 })
